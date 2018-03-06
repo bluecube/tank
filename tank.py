9ab3711e5af79daa75b30c962ee2aa875a3687d1
Kuba Marek <blue.cube@seznam.cz>
2018-03-06 18:39:08 +0100

    A new test print

--- tank.py
+++ tank.py
@@ -5,17 +5,8 @@ import track
 
 import parameters
 
-complete_side_suspension = codecad.Assembly([suspension.left_wheel_suspension.translated_x(-1.5 * parameters.road_wheel_base_spacing),
-                                             suspension.left_wheel_suspension.translated_x(-0.5 * parameters.road_wheel_base_spacing),
-                                             suspension.right_wheel_suspension.translated_x(0.5 * parameters.road_wheel_base_spacing),
-                                             suspension.right_wheel_suspension.translated_x(1.5 * parameters.road_wheel_base_spacing),
-                                             ]).make_part("complete_side_suspension")
-complete_side = codecad.Assembly([complete_side_suspension.translated_z(parameters.road_wheel_diameter / 2 + parameters.track_thickness),
-                                  track.track_row(10).translated_z(parameters.track_thickness / 2)
-                                  ]).make_part("complete_side")
-
-all_suspension = codecad.Assembly([complete_side.translated_y(-80),
-                                   complete_side.rotated_z(180).translated_y(80)])
+test_print = codecad.Assembly([suspension.bogie_assembly.translated_z(track.thickness),
+                               track.track_row(2).translated_x(20)])
 
 if __name__ == "__main__":
-    codecad.commandline_render(all_suspension, 0.2)
+    codecad.commandline_render(test_print.shape().rotated_z(-90), 0.2)
