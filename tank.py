fe698026050a2eee4e8d06c97a64053e7cebfd96
Kuba Marek <blue.cube@seznam.cz>
2018-04-03 10:26:54 +0200

    Overall layout test

---
+++ tank.py
@@ -0,0 +1,16 @@
+import codecad
+
+import suspension
+import transmission
+
+layout = codecad.assembly("tank_layout_test_assembly",
+    [
+    suspension.suspension_assembly_left.hidden(),
+    suspension.suspension_assembly_left.translated_x(suspension.suspension_spacing).hidden(),
+    suspension.suspension_assembly_left.translated_x(2 * suspension.suspension_spacing),
+    transmission.drive_sprocket.rotated_x(90).translated_x(2 * suspension.suspension_spacing + 145)
+    ])
+
+if __name__ == "__main__":
+    codecad.commandline_render(layout, 0.1)
+
