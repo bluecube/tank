10c19462e9baacb7d77fb78bdfa3076d843e433f
Kuba Marek <blue.cube@seznam.cz>
2018-05-11 10:14:07 +0200

    More tensioner wip

--- tensioner.py
+++ tensioner.py
@@ -16,72 +16,107 @@ import track
 # The layout calculations and tensioner assembly are referenced to the following
 # suspension module's main pivot
 
-wheel_max_y = 10
+wheel_diameter = 50
+wheel_base_y = 0 # Approximate y coordinate of the wheel center
+spring_anchor_y = 0
+spring_angle_offset = 60 # degrees
+spring_inversion_safety_distance = 3
 
-wheel_track_guide_radius = suspension.wheel_diameter / 2 - track.guide_height
-worst_wheel_position = Vector(math.cos(suspension.arm_up_angle),
-                                   math.sin(suspension.arm_up_angle)) * suspension.arm_length + \
-                            suspension.get_bogie_wheel_position(suspension.arm_up_angle - suspension.arm_neutral_angle - suspension.bogie_swing_angle,
-                                                                 -1)
-worst_wheel_spring_point = suspension.spring_up_point
+wheel_clearance = suspension.wheel_clearance
+arm_clearance = suspension.arm_clearance
+track_clearance = track.body_clearance
 
-worst_track_guide_points = util.outer_tangent(worst_wheel_spring_point, suspension.arm_thickness / 2 + track.body_clearance,
-                                              worst_wheel_position, wheel_track_guide_radius)
+arm_thickness = suspension.arm_thickness
+arm_length = wheel_diameter / 2 - arm_thickness / 2 - wheel_clearance
+    # Max length so that the pivot is still completely hidden by the wheel
 
-def wheel_back_center_equation(x):
-    dist = util.point_to_line_distance(Vector(x, wheel_max_y),
-                                       worst_track_guide_points[1],
-                                       worst_track_guide_points[0])
-    return dist - wheel_track_guide_radius
+spring_anchor_point = Vector(0 - suspension.arm_pivot_thickness / 2 - vitamins.spring.top_mount_od / 2 - suspension.arm_clearance,
+                             spring_anchor_y)
 
-wheel_back_x = scipy.optimize.brentq(wheel_back_center_equation,
-                                     -100 * suspension.wheel_diameter,
-                                     100 * suspension.wheel_diameter)
-print(wheel_back_x)
-wheel_back_x = min(wheel_back_x,
-                   -math.sqrt((suspension.wheel_diameter / 2 +
-                               suspension.arm_pivot_thickness / 2 +
-                               suspension.wheel_clearance)**2 -
-                              wheel_max_y**2))
-print(wheel_back_x)
+def spring_arm_length_equation(spring_arm_length):
+    return arm_length**2 + spring_arm_length**2 - 2 * arm_length * spring_arm_length * math.cos(math.radians(spring_angle_offset)) - \
+           (wheel_diameter / 2 + wheel_clearance + vitamins.spring.bottom_mount_od / 2)**2
+spring_arm_length = scipy.optimize.brentq(spring_arm_length_equation, 0, 2 * wheel_diameter)
 
-worst_track_guide_points2 = util.outer_tangent(Vector(wheel_back_x, wheel_max_y),
-                                               wheel_track_guide_radius,
-                                               worst_wheel_position,
-                                               wheel_track_guide_radius)
+pivot_to_spring_base = spring_arm_length + vitamins.spring.length - vitamins.spring.travel - spring_inversion_safety_distance
+pivot_y = wheel_base_y + arm_length
+pivot_position = Vector(-math.sqrt(pivot_to_spring_base**2 - pivot_y**2), pivot_y)
+pivot_to_spring_base_angle = -math.degrees(math.asin(pivot_y / pivot_to_spring_base))
 
-spring_anchor_point = Vector(suspension.spring_down_point.x -
-                             (vitamins.spring.diameter - vitamins.spring.top_mount_thickness) / 2 - suspension.arm_clearance -
-                             vitamins.spring.top_mount_od / 2 -
-                             suspension.arm_thickness -
-                             suspension.arm_clearance,
-                             -10)
+def get_arm_angle(spring_length):
+    return pivot_to_spring_base_angle - \
+           math.degrees(math.acos((spring_arm_length**2 + pivot_to_spring_base**2 - spring_length**2) /
+                                  (2 * spring_arm_length * pivot_to_spring_base))) - \
+           spring_angle_offset
 
-o = circle(d=suspension.wheel_diameter) \
-    .translated(wheel_back_x, wheel_max_y)
-o += capsule(worst_track_guide_points[0][0], worst_track_guide_points[0][1],
-             worst_track_guide_points[1][0], worst_track_guide_points[1][1],
-             1)
-o += capsule(worst_track_guide_points2[0][0], worst_track_guide_points2[0][1],
-             worst_track_guide_points2[1][0], worst_track_guide_points2[1][1],
-             2)
-#o += circle(d=suspension.wheel_diameter) \
-#    .translated(worst_wheel_position[0], worst_wheel_position[1])
-o = o.extruded(1).rotated_x(90)
+arm_angle_back = get_arm_angle(vitamins.spring.length - vitamins.spring.travel)
+arm_angle_front = get_arm_angle(vitamins.spring.length)
 
-o = o.make_part("X")
+wheel_position_back = pivot_position + Vector.polar(arm_length, arm_angle_back)
+wheel_position_front = pivot_position + Vector.polar(arm_length, arm_angle_front)
 
-asm = codecad.assembly("tensioner_test", [o, suspension.suspension_assembly_left])
+#worst_track_guide_points = util.outer_tangent(spring_anchor_point, vitamins.spring.top_mount_od / 2 + track.body_clearance,
+#                                              worst_road_wheel_position, suspension.wheel_diameter / 2 - track.guide_height)
+
+#def tangent_to_line(p1, p2, y, r=0):
+#    d = (p2 - p1).normalized()
+#    p1 -= util.perpendicular(d) * r
+#
+#    y -= p1.y
+#
+#    return p1.x + d.x * y / d.y
+
+#wheel_back_x = tangent_to_line(worst_track_guide_points[0], worst_track_guide_points[1], wheel_max_y, wheel_diameter / 2 - track.guide_height)
+
+#def pivot_y_equation(y):
+#    x = tangent_to_line(worst_track_guide_points[0], worst_track_guide_points[1], y, suspension.arm_pivot_thickness / 2 + track.body_clearance)
+#    return math.hypot(x - wheel_back_x, y - wheel_max_y) - (suspension.wheel_diameter + suspension.arm_thickness) / 2 - suspension.wheel_clearance
+
+#pivot_y = scipy.optimize.brentq(pivot_y_equation, spring_anchor_y, wheel_max_y)
+#pivot_x = tangent_to_line(worst_track_guide_points[0], worst_track_guide_points[1], pivot_y, suspension.arm_pivot_thickness / 2 + track.body_clearance)
+
+def preview(arm_angle):
+    wheel_position = pivot_position + Vector.polar(arm_length, arm_angle)
+
+    o = circle(d=wheel_diameter) - circle(d=0.9 * wheel_diameter) + circle(d=2)
+    o = o.translated(wheel_position.x, wheel_position.y)
+
+    o += circle(d=5).translated(pivot_position.x, pivot_position.y)
+    spring_point = pivot_position + Vector.polar(spring_arm_length, arm_angle + spring_angle_offset)
+    o += capsule(spring_anchor_point.x, spring_anchor_point.y, spring_point.x, spring_point.y, vitamins.spring.diameter)
+    return o
+
+#    .translated(wheel_back_x, wheel_max_y)
+#o = capsule(worst_track_guide_points[0][0], worst_track_guide_points[0][1],
+#            worst_track_guide_points[1][0], worst_track_guide_points[1][1],
+#            1)
+#o += circle(d=vitamins.spring.top_mount_od).translated(spring_anchor_point.x, spring_anchor_point.y)
+#o += circle(d=vitamins.spring.top_mount_od).translated(pivot_x, pivot_y)
+#o = o.extruded(1).rotated_x(90)
+
+#o = o.make_part("X")
+
+#asm = codecad.assembly("tensioner_test", [o, suspension.suspension_generator(False, suspension.arm_up_angle),
+                                          #o.translated_z(-100), suspension.suspension_generator(False).translated_z(-100),
+                                          #o.translated_z(-200), suspension.suspension_generator(False, suspension.arm_down_angle).translated_z(-200)])
+
+o = preview(arm_angle_front).translated_y(-50) + preview(arm_angle_back).translated_y(50)
 
 if __name__ == "__main__":
     def p(name, f=lambda x: x):
         print(name, f(globals()[name]))
 
     p("spring_anchor_point")
-    p("worst_wheel_position")
-    p("worst_wheel_spring_point")
-    p("worst_track_guide_points")
-    p("wheel_back_x")
-    p("wheel_max_y")
+    p("worst_road_wheel_position")
+    p("arm_length")
+    p("spring_arm_length")
+    p("pivot_position")
+    p("arm_angle_back")
+    p("wheel_position_back")
+    p("arm_angle_front")
+    p("wheel_position_front")
+    p("pivot_to_spring_base_angle")
+    #p("wheel_back_x")
+    #p("wheel_max_y")
 
-    codecad.commandline_render(asm.rotated_z(10))
+    codecad.commandline_render(o)
