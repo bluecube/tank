f3e3aa5cb669b9bc89e669eecedceccba12d59f1
Kuba Marek <blue.cube@seznam.cz>
2018-06-13 10:00:07 +0200

    Belt tensioner, spring mounting is not designed yet

--- tensioner.py
+++ tensioner.py
@@ -18,11 +18,14 @@ import track
 
 wheel_diameter = 55
 wheel_base_y = 0 # Approximate y coordinate of the wheel center
-spring_anchor_y = 0
-spring_angle_offset = 70 # degrees
-spring_inversion_safety_distance = 2
+spring_anchor_y = 15
+spring_angle_offset = 90 # degrees
+spring_inversion_safety_distance = 4
+
+pivot_round_clearance = suspension.pivot_round_clearance
 
 wheel_screw = vitamins.long_screw # Screw used as an axle for the tensioner wheel
+wheel_bearing = vitamins.small_bearing
 
 wheel_clearance = suspension.wheel_clearance
 arm_clearance = suspension.arm_clearance
@@ -30,9 +33,11 @@ track_clearance = track.body_clearance
 
 arm_width = suspension.arm_width
 arm_thickness = suspension.arm_thickness
-arm_pivot_thickness = suspension.arm_pivot_thickness
-arm_length = wheel_diameter / 2 - arm_pivot_thickness / 2 - wheel_clearance
+
+inner_bearing_height = 7 # How deep is the bearing seated in the inne wheel half
+arm_length = (wheel_diameter / 2 + wheel_bearing.od / 2 + inner_bearing_height) / 2
     # Max length so that the pivot is still completely hidden by the wheel
+arm_pivot_thickness = 2 * (wheel_diameter / 2 - arm_length - wheel_clearance)
 
 spring_anchor_point = Vector(0 - suspension.arm_pivot_thickness / 2 - vitamins.spring.top_mount_od / 2 - suspension.arm_clearance,
                              spring_anchor_y)
@@ -42,15 +47,16 @@ def spring_arm_length_equation(spring_arm_length):
            (wheel_diameter / 2 + wheel_clearance + vitamins.spring.bottom_mount_od / 2)**2
 spring_arm_length = scipy.optimize.brentq(spring_arm_length_equation, 0, 2 * wheel_diameter)
 
-pivot_to_spring_base = spring_arm_length + vitamins.spring.length - vitamins.spring.travel - spring_inversion_safety_distance
+pivot_to_spring_anchor = spring_arm_length + vitamins.spring.length - vitamins.spring.travel - spring_inversion_safety_distance
 pivot_y = wheel_base_y + arm_length
-pivot_position = Vector(-math.sqrt(pivot_to_spring_base**2 - pivot_y**2), pivot_y)
-pivot_to_spring_base_angle = -math.degrees(math.asin(pivot_y / pivot_to_spring_base))
+pivot_position = Vector(spring_anchor_point.x - math.sqrt(pivot_to_spring_anchor**2 - (pivot_y - spring_anchor_y)**2),
+                        pivot_y)
+pivot_to_spring_anchor_angle = -math.degrees(math.asin((pivot_y - spring_anchor_y) / pivot_to_spring_anchor))
 
 def get_arm_angle(spring_length):
-    return pivot_to_spring_base_angle - \
-           math.degrees(math.acos((spring_arm_length**2 + pivot_to_spring_base**2 - spring_length**2) /
-                                  (2 * spring_arm_length * pivot_to_spring_base))) - \
+    return pivot_to_spring_anchor_angle - \
+           math.degrees(math.acos((spring_arm_length**2 + pivot_to_spring_anchor**2 - spring_length**2) /
+                                  (2 * spring_arm_length * pivot_to_spring_anchor))) - \
            spring_angle_offset
 
 arm_angle_back = get_arm_angle(vitamins.spring.length - vitamins.spring.travel)
@@ -138,7 +144,8 @@ def wheel_generator(diameter, whole_width,
 def arm_generator(thickness, pivot_thickness, width,
                   arm_length, spring_arm_length, spring_angle_offset,
                   cone_height,
-                  wheel_bearing):
+                  pivot_hole_diameter,
+                  wheel_bearing, wheel_screw):
 
     cone_upper_diameter = wheel_bearing.id + 2 * wheel_bearing.shoulder_size
 
@@ -163,42 +170,22 @@ def arm_generator(thickness, pivot_thickness, width,
                       lower_diameter=(cone_upper_diameter + thickness) / 2 + cone_height,
                       base_height=width) \
         .translated_x(arm_length)
-    cone &= outline.extruded(float("inf"))
+    arm += cone & outline.extruded(float("inf"))
 
-    arm += cone
+
+    arm -= cylinder(d=pivot_hole_diameter, h=float("inf"))
+
+    arm -= cylinder(d=wheel_screw.diameter, h=float("inf")).translated_x(arm_length)
+    arm -= regular_polygon2d(n=6, across_flats=wheel_screw.lock_nut.s) \
+        .extruded(2 * wheel_screw.lock_nut.height) \
+        .translated_x(arm_length)
 
     return arm
 
-def preview(arm_angle):
-    wheel_position = pivot_position + Vector.polar(arm_length, arm_angle)
-
-    o = circle(d=wheel_diameter) - circle(d=0.9 * wheel_diameter) + circle(d=2)
-    o = o.translated(wheel_position.x, wheel_position.y)
-
-    o += circle(d=5).translated(pivot_position.x, pivot_position.y)
-    spring_point = pivot_position + Vector.polar(spring_arm_length, arm_angle + spring_angle_offset)
-    o += capsule(spring_anchor_point.x, spring_anchor_point.y, spring_point.x, spring_point.y, vitamins.spring.diameter)
-    return o
-
-#o = capsule(worst_track_guide_points[0][0], worst_track_guide_points[0][1],
-#            worst_track_guide_points[1][0], worst_track_guide_points[1][1],
-#            1)
-#o += circle(d=vitamins.spring.top_mount_od).translated(spring_anchor_point.x, spring_anchor_point.y)
-#o += circle(d=vitamins.spring.top_mount_od).translated(pivot_x, pivot_y)
-#o = o.extruded(1).rotated_x(90)
-
-#o = o.make_part("X")
-
-#asm = codecad.assembly("tensioner_test", [o, suspension.suspension_generator(False, suspension.arm_up_angle),
-                                          #o.translated_z(-100), suspension.suspension_generator(False).translated_z(-100),
-                                          #o.translated_z(-200), suspension.suspension_generator(False, suspension.arm_down_angle).translated_z(-200)])
-
-o = preview(arm_angle_front).translated_y(-50) + preview(arm_angle_back).translated_y(50)
-
 inner_wheel_half = wheel_generator(wheel_diameter, suspension.wheel_width,
                                    0.1, # Crown tolerance
-                                   vitamins.small_bearing,
-                                   7, # bearing_height
+                                   wheel_bearing,
+                                   inner_bearing_height,
                                    4 * parameters.extrusion_width,
                                    track.guide_height, track.guide_width, track.guide_side_angle, track.clearance,
                                    arm_length, vitamins.shoulder_screw.head_diameter, vitamins.shoulder_screw.head_height - wheel_clearance, wheel_clearance,
@@ -206,24 +193,71 @@ inner_wheel_half = wheel_generator(wheel_diameter, suspension.wheel_width,
     .make_part("inner_tensioner_wheel", ["3d_print"])
 outer_wheel_half = wheel_generator(wheel_diameter, suspension.wheel_width,
                                    0.1, # Crown tolerance
-                                   vitamins.small_bearing,
+                                   wheel_bearing,
                                    suspension.wheel_width + wheel_clearance + arm_width - wheel_screw.length,
                                    4 * parameters.extrusion_width,
                                    track.guide_height, track.guide_width, track.guide_side_angle, track.clearance,
                                    arm_length, vitamins.shoulder_screw.head_diameter, vitamins.shoulder_screw.head_height - wheel_clearance, wheel_clearance,
                                    False) \
     .make_part("outer_tensioner_wheel", ["3d_print"])
+arm_left = arm_generator(arm_thickness, arm_pivot_thickness, arm_width,
+                         arm_length, spring_arm_length, spring_angle_offset,
+                         inner_bearing_height + wheel_clearance,
+                         vitamins.shoulder_screw.diameter2 + pivot_round_clearance,
+                         wheel_bearing, wheel_screw) \
+    .make_part("tensioner_arm_left", ["3d_print"])
+arm_right = arm_left.shape().mirrored_x().make_part("tensioner_arm_right", ["3d_print"])
+
 wheel_assembly = codecad.assembly("tensioner_wheel_assembly",
-                                  [inner_wheel_half.rotated_x(90).translated_y(8 + suspension.wheel_width / 2),
-                                   outer_wheel_half.rotated_x(-90).translated_y(-suspension.wheel_width / 2),
-                                   wheel_screw] +
-                                  [vitamins.small_bearing] * 2)
+                                  [inner_wheel_half.rotated_x(90),
+                                   outer_wheel_half.rotated_x(-90).translated_y(-suspension.wheel_width)] +
+                                  [wheel_bearing] * 2)
 
-arm = arm_generator(arm_thickness, arm_pivot_thickness, arm_width,
-                  arm_length, spring_arm_length, spring_angle_offset,
-                  9,
-                  vitamins.small_bearing)
+def tensioner_generator(right, arm_angle = (arm_angle_back + arm_angle_front) / 2):
+    spring_point = Vector.polar(spring_arm_length, arm_angle + spring_angle_offset) + pivot_position
 
+    v = spring_point.flattened() - spring_anchor_point.flattened()
+    length = abs(v)
+
+    spring_degrees = 90 - math.degrees(math.atan2(v.y, v.x))
+    spring = vitamins.spring(length)
+
+    if right:
+        arm = arm_right
+        multiplier = 1
+    else:
+        arm = arm_left.rotated_y(180)
+        multiplier = -1
+
+    arm_angle = -arm_angle
+
+    asm = codecad.assembly("tensioner_assembly_" + ("right" if right else "left"),
+                           [arm \
+                                .rotated_x(-90) \
+                                .rotated_y(180) \
+                                .rotated_y(arm_angle) \
+                                .translated(pivot_position.x, pivot_position.z, pivot_position.y),
+                            wheel_assembly \
+                                .translated_y(-arm_width - wheel_clearance) \
+                                .rotated_z(90 + 90 * multiplier) \
+                                .translated_x(arm_length) \
+                                .rotated_y(arm_angle) \
+                                .translated(pivot_position.x, pivot_position.z, pivot_position.y),
+                            spring \
+                                .rotated_y(spring_degrees) \
+                                .translated(spring_anchor_point.x,
+                                            multiplier * (spring_anchor_point.z + vitamins.spring.top_mount_thickness / 2),
+                                            spring_anchor_point.y),
+                            wheel_screw,
+                            wheel_screw.lock_nut,
+                            vitamins.shoulder_screw,
+                            vitamins.shoulder_screw.lock_nut])
+
+    return asm
+
+
+tensioner_assembly_left = tensioner_generator(False)
+tensioner_assembly_right = tensioner_generator(True)
 
 if __name__ == "__main__":
     def p(name, f=lambda x: x):
@@ -237,8 +271,12 @@ if __name__ == "__main__":
     p("wheel_position_back")
     p("arm_angle_front")
     p("wheel_position_front")
-    p("pivot_to_spring_base_angle")
+    p("pivot_to_spring_anchor_angle")
     p("wheel_travel")
     p("wheel_x_travel")
 
-    codecad.commandline_render((wheel_assembly.rotated_x(-90).shape() & half_space()).rotated_x(-90))
+    codecad.commandline_render(tensioner_generator(False, arm_angle_back).shape().rotated_z(180)
+                               + tensioner_generator(False, arm_angle_front).shape().rotated_z(180).translated_z(70)
+                               + tensioner_generator(True, arm_angle_back).shape()
+                               + tensioner_generator(True, arm_angle_front).shape().translated_z(70))
+    #codecad.commandline_render(wheel_assembly)
